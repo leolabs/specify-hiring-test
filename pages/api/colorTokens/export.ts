040bcc93446d26eb5fa87aae64aa9b4eee6e77b8
Leo Bernard <admin@leolabs.org>
2021-11-04 17:06:35 +0100

    :sparkles: Implement an export endpoint

---
+++ pages/api/colorTokens/export.ts
@@ -0,0 +1,47 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { parsers } from "../../../util/parsers";
+import prisma from "../../../util/prisma";
+import { dbToColorToken } from "../../../util/schemas/colorToken";
+
+/**
+ * Exports all color tokens to a given format.
+ * Downloads the file if you pass it a "download" param.
+ */
+const exportColorTokens = async (req: NextApiRequest, res: NextApiResponse) => {
+  const parser = parsers[req.query.type as string];
+
+  if (!parser) {
+    res.status(400).json({
+      message: "Missing or invalid type query parameter",
+    });
+    return;
+  }
+
+  const dbColorTokens = await prisma.colorToken.findMany();
+  const colorTokens = dbColorTokens.map(dbToColorToken);
+  const parsed = parser.parse(colorTokens);
+
+  if ("download" in req.query) {
+    // This tells the browser to download the
+    // file instead of showing its contents.
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename="${parser.fileName}"`
+    );
+  }
+
+  res.setHeader("Content-Type", parser.mimeType).send(parsed);
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      await exportColorTokens(req, res);
+      break;
+    default:
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
