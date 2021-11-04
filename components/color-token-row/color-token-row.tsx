1550bb327ff1252524eed34fc0a0185112ca0794
Leo Bernard <admin@leolabs.org>
2021-11-04 23:06:22 +0100

    :sparkles: Implement a way to delete tokens

--- components/color-token-row/color-token-row.tsx
+++ components/color-token-row/color-token-row.tsx
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Text } from "@chakra-ui/layout";
-import { HStack, Spacer } from "@chakra-ui/react";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  HStack,
+  IconButton,
+  Spacer,
+} from "@chakra-ui/react";
 import { ColorToken } from "@prisma/client";
 
 import { colorToRgba } from "../../util/color";
 import { dbToColorToken } from "../../util/schemas/color-token";
 import { ColorBox } from "./color-box";
+import { DeleteIcon } from "@chakra-ui/icons";
+import { useSWRConfig } from "swr";
+import { ColorTokenList } from "../../pages/api/colorTokens";
+import { DeleteDialog } from "./delete-dialog";
 
 interface RowProps {
   colorToken: ColorToken;
 }
 
 export const ColorTokenRow: React.FC<RowProps> = ({ colorToken }) => {
+  const { mutate } = useSWRConfig();
   const { value } = dbToColorToken(colorToken);
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+
+  const deleteRow = async () => {
+    mutate(
+      "/api/colorTokens",
+      (l: ColorTokenList) => ({
+        colorTokens: l.colorTokens.filter((t) => t.id !== colorToken.id),
+      }),
+      false
+    );
+
+    await fetch(`/api/colorTokens/${colorToken.id}`, { method: "DELETE" });
+  };
 
   return (
     <HStack
@@ -30,6 +59,19 @@ export const ColorTokenRow: React.FC<RowProps> = ({ colorToken }) => {
         {colorToken.name}
       </Text>
       <Text opacity="0.5">{colorToRgba(value)}</Text>
+      <Spacer />
+      <IconButton
+        onClick={() => setShowDeleteDialog(true)}
+        variant="ghost"
+        icon={<DeleteIcon />}
+        aria-label="Delete this token"
+      />
+      <DeleteDialog
+        isOpen={showDeleteDialog}
+        colorToken={colorToken}
+        onCancel={() => setShowDeleteDialog(false)}
+        onDelete={deleteRow}
+      />
     </HStack>
   );
 };
---
+++ components/color-token-row/delete-dialog.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { ColorToken } from "@prisma/client";
+import { Button } from "@chakra-ui/button";
+import {
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+} from "@chakra-ui/modal";
+
+interface DeleteProps {
+  colorToken: ColorToken;
+  isOpen: boolean;
+  onDelete: () => void;
+  onCancel: () => void;
+}
+
+export const DeleteDialog: React.FC<DeleteProps> = ({
+  colorToken,
+  isOpen,
+  onDelete,
+  onCancel,
+}) => {
+  const cancelRef = React.useRef<HTMLButtonElement>();
+
+  return (
+    <AlertDialog
+      isOpen={isOpen}
+      leastDestructiveRef={cancelRef}
+      onClose={onCancel}
+    >
+      <AlertDialogOverlay>
+        <AlertDialogContent>
+          <AlertDialogHeader fontSize="lg" fontWeight="bold">
+            Delete {colorToken.name}?
+          </AlertDialogHeader>
+
+          <AlertDialogBody>
+            Are you sure? You can't undo this action afterwards.
+          </AlertDialogBody>
+
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onCancel}>
+              Cancel
+            </Button>
+            <Button colorScheme="red" onClick={onDelete} ml={3}>
+              Delete
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  );
+};
