c0d6bff21009a2212fb62eaa731115c9fc8d1d8b
Leo Bernard <admin@leolabs.org>
2021-11-04 15:54:13 +0100

    :sparkles: Implement a ColorToken type that follows the spec

---
+++ util/schemas/colorToken.ts
@@ -0,0 +1,48 @@
+import { ColorToken as DbColorToken } from ".prisma/client";
+
+/** A single RGBA color vector */
+export interface Color {
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+}
+
+/**
+ * A single, spec-compliant color token.
+ *
+ * Example:
+ * ```
+ * {
+ *   "name": "Colors/Accent",
+ *   "value": {
+ *     "a": 1,
+ *     "b": 254,
+ *     "g": 124,
+ *     "r": 87
+ *   },
+ *   "meta": {
+ *     "source": "localStyles"
+ *   }
+ * }
+ * ```
+ */
+export interface ColorToken {
+  name: string;
+  value: Color;
+  meta?: Record<string, any>;
+}
+
+/**
+ * Converts the schema of a color token stored in the db into a spec-compliant object
+ */
+export const dbToColorToken = (dbColorToken: DbColorToken): ColorToken => ({
+  name: dbColorToken.name,
+  value: {
+    r: dbColorToken.colorRed,
+    g: dbColorToken.colorGreen,
+    b: dbColorToken.colorBlue,
+    a: dbColorToken.colorAlpha,
+  },
+  meta: (dbColorToken.meta as any) ?? undefined,
+});
