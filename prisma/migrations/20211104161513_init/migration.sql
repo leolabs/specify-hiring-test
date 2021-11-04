802f4b3c7315d61bbdb3a1bbf30f14e712b0063d
Leo Bernard <admin@leolabs.org>
2021-11-04 17:25:39 +0100

    :card_file_box: Improve database schema

--- prisma/migrations/20211104161513_init/migration.sql
+++ prisma/migrations/20211104161513_init/migration.sql
@@ -5,8 +5,8 @@ CREATE TABLE "ColorToken" (
     "colorRed" INTEGER NOT NULL,
     "colorGreen" INTEGER NOT NULL,
     "colorBlue" INTEGER NOT NULL,
-    "colorAlpha" DECIMAL(65,30) NOT NULL,
-    "metadata" JSONB,
+    "colorAlpha" DOUBLE PRECISION NOT NULL,
+    "meta" JSONB,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
