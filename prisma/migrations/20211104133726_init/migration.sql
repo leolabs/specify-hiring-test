ee5e989577b8a47d1663ffc98e690efd183082d5
Leo Bernard <admin@leolabs.org>
2021-11-04 15:42:44 +0100

    :card_file_box: Work with ints and floats for color values and make metadata optional

--- prisma/migrations/20211104133726_init/migration.sql
+++ prisma/migrations/20211104133726_init/migration.sql
@@ -2,11 +2,11 @@
 CREATE TABLE "ColorToken" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "colorRed" DECIMAL(65,30) NOT NULL,
-    "colorGreen" DECIMAL(65,30) NOT NULL,
-    "colorBlue" DECIMAL(65,30) NOT NULL,
+    "colorRed" INTEGER NOT NULL,
+    "colorGreen" INTEGER NOT NULL,
+    "colorBlue" INTEGER NOT NULL,
     "colorAlpha" DECIMAL(65,30) NOT NULL,
-    "metadata" JSONB NOT NULL,
+    "metadata" JSONB,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
