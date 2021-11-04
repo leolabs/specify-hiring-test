ad3926f71afb23099ead7d6dd49b0cbe9d2386f4
Leo Bernard <admin@leolabs.org>
2021-11-04 15:53:40 +0100

    :sparkles: Implement some string manipulation utils

---
+++ util/strings.test.ts
@@ -0,0 +1,59 @@
+import {
+  kebapCase,
+  camelCase,
+  pascalCase,
+  extractWords,
+  indentLines,
+} from "./strings";
+
+describe("extractWords", () => {
+  it("should extract words from a string", () => {
+    expect(extractWords("Hello World")).toEqual(["hello", "world"]);
+    expect(extractWords("Hello/World")).toEqual(["hello", "world"]);
+    expect(extractWords("Hello-World")).toEqual(["hello", "world"]);
+  });
+});
+
+describe("pascalCase", () => {
+  it("should return a string with the first letter uppercased", () => {
+    expect(pascalCase("test")).toBe("Test");
+  });
+
+  it("should return a string with the first letter uppercased", () => {
+    expect(pascalCase("test test")).toBe("TestTest");
+  });
+});
+
+describe("camelCase", () => {
+  it("should return a string with the first letter lowercased", () => {
+    expect(camelCase("test")).toBe("test");
+  });
+
+  it("should return a string with the first letter lowercased", () => {
+    expect(camelCase("test test")).toBe("testTest");
+  });
+});
+
+describe("kebapCase", () => {
+  it("should return a string with the first letter lowercased", () => {
+    expect(kebapCase("test")).toBe("test");
+  });
+
+  it("should return a string with the first letter lowercased", () => {
+    expect(kebapCase("test test")).toBe("test-test");
+  });
+});
+
+describe("indentLines", () => {
+  it("should indent a single line lines", () => {
+    expect(indentLines("test", 2)).toBe("  test");
+  });
+
+  it("should indent multiple lines", () => {
+    expect(indentLines("test\ntest", 2)).toBe("  test\n  test");
+  });
+
+  it("should trim leading whitespace", () => {
+    expect(indentLines("test\n  test", 2)).toBe("  test\n  test");
+  });
+});
---
+++ util/strings.ts
@@ -0,0 +1,35 @@
+/**
+ * Extracts an array of words from a given string,
+ * trimming whitespace and removing empty strings.
+ *
+ * @example `"Colors/Accent "` -> `["colors", "accent"]`
+ */
+export const extractWords = (str: string) =>
+  str
+    .split(/[-_\s\/]/)
+    .map((s) => s.toLowerCase().trim())
+    .filter(Boolean);
+
+/** Capitalizes the first letter of a string */
+export const capitalize = (str: string) =>
+  str.charAt(0).toUpperCase() + str.slice(1);
+
+/** Converts a string to PascalCase */
+export const pascalCase = (str: string) =>
+  extractWords(str).map(capitalize).join("");
+
+/** Converts a string to camelCase */
+export const camelCase = (str: string) =>
+  extractWords(str)
+    .map((w, i) => (i > 0 ? capitalize(w) : w))
+    .join("");
+
+/** Converts a string to kebap-case */
+export const kebapCase = (str: string) => extractWords(str).join("-");
+
+/** Indents all lines of a string to a given indentation level */
+export const indentLines = (str: string, indentation = 2) =>
+  str
+    .split("\n")
+    .map((s) => " ".repeat(indentation) + s.trimStart())
+    .join("\n");
